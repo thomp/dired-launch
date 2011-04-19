6f50ad3dc7937b2fedeffcad060d7d179125e61d
D. Thompson <thompdump@gmail.com>
2011-04-18 17:10:44 -0700

    Support multiple concurrent launched apps.

--- dired-launch.el
+++ dired-launch.el
@@ -6,7 +6,38 @@
 ;; - see also:
 ;;     http://omniorthogonal.blogspot.com/2008/05/useful-emacs-dired-launch-hack.html
 ;;     http://www.emacswiki.org/emacs/AnythingLauncher
-;;
+;;     ORG-OPEN-AT-POINT
+
+
+;; using this is problematic since only one process can run at a time (issue: inability to specify distinct async output buffers for each process...)
+(defun dired-launch-builtin
+  ;; dired-do-async-shell-command has issues... but plays nicely with xdg-open
+  ;; dired-do-shell-command is a drag since emacs locks until launched app terminates
+  (dired-do-async-shell-command
+   (case system-type
+     (gnu/linux "xdg-open") ; kde-open, gnome-open, ...
+     (darwin "open"))
+   nil
+   (dired-get-marked-files t current-prefix-arg)))
+
+;; workaround for DIRED-LAUNCH-BUILTIN's inability to handle more than one process at a time
+(defun dired-launch-homebrew ()
+  (let ((launch-cmd 	   
+	 (case system-type
+	   (gnu/linux "xdg-open")	; kde-open, gnome-open, ...
+	   (darwin "open")))
+	(files (dired-get-marked-files t current-prefix-arg)))
+    (mapc #'(lambda (file)
+		 (let ((cmd 
+			(concat
+			 launch-cmd
+			 " "
+			 (shell-quote-argument file))))
+		   (start-process-shell-command 
+		    cmd
+		    nil			  ; don't associate process w/a buffer
+		    cmd)))
+	 files)))
 
 (defun dired-launch-command ()
   (interactive) 
@@ -15,14 +46,8 @@
        (w32-shell-execute "open" (dired-get-filename) nil 1))
     ;; forego persistent *Async Shell Command* buffer
     (save-window-excursion
-    ;; dired-do-async-shell-command has issues... but plays nicely with xdg-open
-    ;; dired-do-shell-command is a drag since emacs locks until launched app terminates
-      (dired-do-async-shell-command
-       (case system-type
-	 (gnu/linux "xdg-open") ; kde-open, gnome-open, ...
-	 (darwin "open"))
-       nil
-       (dired-get-marked-files t current-prefix-arg)))))
+      (dired-launch-homebrew)
+      )))
 
 (setq dired-load-hook
       (lambda (&rest ignore)
