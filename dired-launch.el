7c0530af57e71f9dfda81d92c34f1dd67aefa18b
D Thompson <thompdump@gmail.com>
2012-04-02 19:28:32 -0700

    Add basic instructions to README. Other minor revisions.

--- dired-launch.el
+++ dired-launch.el
@@ -1,28 +1,20 @@
 ;;
-;; dired-launch.el: a dired tweak -> use dired as a launcher
+;; dired-launch.el: use dired as a launcher
 ;;
 
 ;;
 ;; use:
-;; - launch files with 'l' key
-;; - change preferred application with 'mimeopen -a myfile'
+;; - launch file with the 'l' key
+;; - change a preferred application with 'mimeopen -d myfile'
 ;;
 
 
 ;; DL-MAILCAP-FRIEND defines program and associated argument(s)
 (defvar dl-mailcap-friend
-  ;; "exo-open"
-  ;; GNOME-OPEN
-  ;; KDE-OPEN
-  '("mimeopen" "-n")
-  ;; XDG-OPEN 
-  ;; - issues w/filenames with spaces (see https://bugs.archlinux.org/task/19305, https://bugs.launchpad.net/xdg-utils/+bug/220750)
-  )
+  '("mimeopen" "-n"))
 
-;; using this is problematic since only one process can run at a time (issue: inability to specify distinct async output buffers for each process...)
 (defun dired-launch-builtin
-  ;; dired-do-async-shell-command has issues... but plays nicely with xdg-open
-  ;; dired-do-shell-command is a drag since emacs locks until launched app terminates
+  ;; using this is problematic since only one process can run at a time (issue: inability to specify distinct async output buffers for each process...)
   (dired-do-async-shell-command
    (case system-type
      (gnu/linux dl-mailcap-friend)
@@ -58,7 +50,6 @@
   (if (eq system-type 'windows)
       (dired-map-over-marks
        (w32-shell-execute "open" (dired-get-filename) nil 1))
-    ;; forego persistent *Async Shell Command* buffer
     (save-window-excursion
       (dired-launch-homebrew))))
 
