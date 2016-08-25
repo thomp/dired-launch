4fd1586ed9b1c3a9c160b4415c782ad3142b9f9f
David A. Thompson <thomp@d630>
2016-08-25 15:54:36 -0700

    Add ability to specify launch program; other, minor revisions
    - reduce verbosity
    
    - add README.md
    
    - misc. code cleanup

--- dired-launch.el
+++ dired-launch.el
@@ -26,27 +26,19 @@
   (let ((launch-cmd 	   
 	 (case system-type
 	   (gnu/linux (first dl-mailcap-friend))
-	   (darwin "open")))
-	
-	;; (files		;(dired-get-marked-files t current-prefix-arg)
-	;;  )
-	)
+	   (darwin "open"))))
     (mapc #'(lambda (file)
-	      (let ((buffer-name "dired-launch-output-buffer")
-		    (cmd 
-		     (concat
-		      launch-cmd
-		      " "
-		      (shell-quote-argument file))))
-		(message cmd) 
-		;; handle file names with spaces
-		(call-process launch-cmd
-			      nil	; infile
-			      0 ; async-ish...
-			      nil 
-			      (second dl-mailcap-friend) file
-			      )))
+	      (let ((buffer-name "dired-launch-output-buffer"))
+		(dired-launch-call-process-on launch-cmd file)))
 	  files)))
+
+(defun dired-launch-call-process-on (launch-cmd file)
+  ;; handle file names with spaces
+  (call-process launch-cmd
+		nil	; infile
+		0 ; async-ish...
+		nil 
+		(second dl-mailcap-friend) file))
 
 (defun dired-launch-command ()
   (interactive) 
@@ -55,10 +47,24 @@
        (w32-shell-execute "open" (dired-get-filename) nil 1))
     (save-window-excursion
       (dired-launch-homebrew
-       (dired-get-marked-files t current-prefix-arg)
-       ))))
+       (dired-get-marked-files t current-prefix-arg)))))
 
+(defun dired-launch-with-prompt-command ()
+  (interactive) 
+  (if (eq system-type 'windows) 
+      (message "Windows not supported")
+    (save-window-excursion
+      (mapc #'(lambda (file)
+		(let ((launch-cmd (read-from-minibuffer (concat "Launch " file " with?" ))))
+		  (dired-launch-call-process-on launch-cmd file))) 
+	    (dired-get-marked-files t current-prefix-arg)))))
+  
 (setq dired-load-hook
       (lambda (&rest ignore)
-	(define-key dired-mode-map
-	  "l" 'dired-launch-command)))
+	(define-key dired-mode-map "l" 'dired-launch-command)
+	(define-key dired-mode-map "L" 'dired-launch-with-prompt-command)))
+
+;; anticipate possibility that dired-load-hook (as defined above) was not invoked (e.g., dired loaded already)
+(when (boundp 'dired-mode-map)
+  (define-key dired-mode-map "l" 'dired-launch-command)
+  (define-key dired-mode-map "L" 'dired-launch-with-prompt-command))
