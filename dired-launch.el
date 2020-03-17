866c28008bc3b3de1ca472ed2af1bd09060a1a9b
David A. Thompson <thompdump@gmail.com>
2020-03-17 14:30:59 -0700

    Updates somewhat dusty copyright line

--- dired-launch.el
+++ dired-launch.el
@@ -1,6 +1,6 @@
 ;;; dired-launch.el --- Use dired as a launcher
 
-;; Copyright (C) 2016 David Thompson
+;; Copyright (C) 2016-2019 David Thompson
 ;; Author: David Thompson
 ;; Version: 0.2
 ;; Keywords: dired, launch
